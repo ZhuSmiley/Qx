a9013854e09e323c007db00ccf57622b1a2e1d5f
ZhuSmiley <87383704+ZhuSmiley@users.noreply.github.com>
2021-10-01 23:43:41 +0800

    Update AppMonitor.js

--- Js/AppMonitor.js
+++ Js/AppMonitor.js
@@ -1,36 +1,15 @@
 /*
 【app版本及价格监控】修改自t.me/QuanXApp群友分享 
 Modified by evilbutcher
-
 【仓库地址】https://github.com/evilbutcher/Quantumult_X/tree/master（欢迎star🌟）
-
 【BoxJs】https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/evilbutcher.boxjs.json
+#nPlayer 1116905928
 
-【致谢】
-感谢来自t.me/QuanXApp群友分享脚本！
-感谢Peng-YM的OpenAPI.js！
-
-⚠️【免责声明】
-------------------------------------------
-1、此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
-2、由于此脚本仅用于学习研究，您必须在下载后 24 小时内将所有内容从您的计算机或手机或任何存储设备中完全删除，若违反规定引起任何事件本人对此均不负责。
-3、请勿将此脚本用于任何商业或非法目的，若违反规定请自行对此负责。
-4、此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
-5、本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
-6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
-7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
 
 【Quantumult X】
 ————————————————
 30 7-22 * * * https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/appstore/AppMonitor.js, tag=App价格监控
 
-【Surge】
-————————————————
-App价格监控 = type=cron,cronexp="30 7-22 * * *",script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/appstore/AppMonitor.js,wake-system=true,timeout=600
-
-【Loon】
-————————————————
-cron "30 7-22 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/appstore/AppMonitor.js, timeout=600, tag=App价格监控
 
 app可单独设置区域，未单独设置区域，则采用reg默认区域
 设置区域方式apps=["1443988620:hk","1443988620/us","1443988620-uk","1443988620_jp","1443988620 au"]
@@ -40,7 +19,7 @@ app可单独设置区域，未单独设置区域，则采用reg默认区域
 const $ = new API("App价格监控");
 let apps = [
   "1443988620|hk",
-  "1312014438 cn",
+  "1116905928 cn",
   "499470113/vn",
   "1314212521-jp",
   "1282297037_au",
