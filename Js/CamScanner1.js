bb632436543e059618588234811f733edf051df9
unidea <87383704+unidea@users.noreply.github.com>
2023-10-20 10:18:14 +0800

    更新 CamScanner1.js

--- Js/CamScanner1.js
+++ Js/CamScanner1.js
@@ -9,25 +9,6 @@ Quantumult X:
 
 [mitm]
 hostname = ap*.intsig.net
-
-***************************
-Surge4 or Loon:
-
-[Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-
-[MITM]
-hostname = ap*.intsig.net
-
-***************************
-Quantumult:  
-
-[REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
-
-[MITM]
-hostname = ap*.intsig.net
-
 **************************/
 let obj = JSON.parse($response.body);
 obj = {"data":{"psnl_vip_property":{"expiry":"1750731200"}}};
